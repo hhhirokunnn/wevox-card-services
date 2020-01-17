23bc9f373b51ab036949ef0fddec25fc805c0fbe
hhhirokunnn <m2017hiro@gmail.com>
2020-01-17 12:06:46 +0900

    add rails

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,8 @@
+class HelloController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  def index
+    render status: 200, json: { status: 200, message: 'created', data: {} }
+  end
+end
